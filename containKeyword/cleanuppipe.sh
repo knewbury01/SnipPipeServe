2ccabcd2f06c53f9d761336cf83c80b99846e589
Kristen Newbury <knewbury@ualberta.ca>
2019-04-03 12:09:46 -0600

    add cleanup for pipeline so can reattempt

---
+++ containKeyword/cleanuppipe.sh
@@ -0,0 +1,13 @@
+######################
+#
+# cleans up the files
+# produced from prev pipeline run
+#
+#######################
+
+rm fails/*
+rm compileClass/*
+rm compileMethodClass/*
+rm outputs/*
+rm X*
+rm classes/*
