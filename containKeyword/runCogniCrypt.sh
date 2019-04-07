a3cec4ffb79b20f2520fb3ed995f4a6e61657d32
Kristen Newbury <knewbury@ualberta.ca>
2019-04-07 00:47:13 -0600

    add run cogni loop to run over the original compiled posts

--- containKeyword/runCogniCrypt.sh
+++ containKeyword/runCogniCrypt.sh
@@ -19,11 +19,18 @@ for file in compileMethodClass/*.class; do
     justfile=${file#compileMethodClass/}
     echo "RUNNING ON $justfile"
     cp $file isolateclass/$justfile
-    java -cp /Users/knewbury/Desktop/CryptoAnalysis/CryptoAnalysis/build/CryptoAnalysis-
-2.1-SNAPSHOT-jar-with-dependencies.jar crypto.HeadlessCryptoScanner --rulesDir=/Users/kn
-ewbury/Desktop/CryptoAnalysis/JCA_rules --applicationCp=/Users/knewbury/Desktop/snippets
-/containKeyword/isolateclass ${justfile%.class} &> CogniCryptResults/${justfile%.class}.
-txt
+    java -cp /Users/knewbury/Desktop/CryptoAnalysis/CryptoAnalysis/build/CryptoAnalysis-2.1-SNAPSHOT-jar-with-dependencies.jar crypto.HeadlessCryptoScanner --rulesDir=/Users/knewbury/Desktop/CryptoAnalysis/JCA_rules --applicationCp=/Users/knewbury/Desktop/snippets/containKeyword/isolateclass ${justfile%.class} &> CogniCryptResults/${justfile%.class}.txt
     rm isolateclass/$justfile
 
 done
+
+for file in Original/*.class; do
+
+    justfile=${file#Original/}
+    echo "RUNNING ON $justfile"
+    cp $file isolateclass/$justfile
+
+    java -cp /Users/knewbury/Desktop/CryptoAnalysis/CryptoAnalysis/build/CryptoAnalysis-2.1-SNAPSHOT-jar-with-dependencies.jar crypto.HeadlessCryptoScanner --rulesDir=/Users/knewbury/Desktop/CryptoAnalysis/JCA_rules --applicationCp=/Users/knewbury/Desktop/snippets/containKeyword/isolateclass ${justfile%.class} &> CogniCryptResults/${justfile%.class}.txt
+    rm isolateclass/$justfile
+    
+    done
