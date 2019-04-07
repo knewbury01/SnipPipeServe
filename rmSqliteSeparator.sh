ba443e6cd9d73711a77f010c0890dd4d1d2d4624
Kristen Newbury <knewbury@ualberta.ca>
2019-04-07 00:45:30 -0600

    add rmsqliteop step to auto perform in splitsnips step

--- rmSqliteSeparator.sh
+++ rmSqliteSeparator.sh
@@ -1,14 +1,15 @@
 #############################
-#WARNING ONLY RUN THIS ONCE PER FILESET
+#WARNING THIS IS RUN FROM splitsnippets.sh
+#THUS IT SHOULD NOT BE CALLED MANUALLY
 #
 # this rm's the "|" character in each file bc of sqlite output
 # BUT it rms the FIRST OCCURRENCE
 # so if ran twice on same file can result in removing
 # a character that was actually supposed to be there
 #############################
-for file in *.java; do
+for file in xx*.java; do
 
-
+    #for linux: sed -i 's/|//' $file
     sed -i '' 's/|//' $file
 
 done
--- splitsnippets.sh
+++ splitsnippets.sh
@@ -6,12 +6,17 @@
 #
 #
 # REQUIRES GNU csplit
-# BSD split limit behaviour is different 
+# BSD split limit behaviour is different
+#
+# usage:
+#    ./splitsnippets.sh <file-with-all-snippet-content>
 ##########################
 
 
 #first use the split identifier to split content into uniqe code snippets 
 echo "SPLITTING $1"
+#currently config'd for this exact dataset
+#should be able to use {*} (untested)
 csplit -n 5 $1 '/---UNIQUEIDFORSNIP---/' {1407} #&>/dev/null
 
 #then remove this identifier
@@ -20,6 +25,9 @@ do
     #for linux: sed -i 's/---UNIQUEIDFORSNIP---//' $file
     sed -i '' 's/---UNIQUEIDFORSNIP---//' $file    
 done
+
+#removes the last | sqlite separator in this data
+./rmSqliteSeparator.sh
 
 echo -n "Created this many snippets: "
 find . -name "xx*" | wc -l 
