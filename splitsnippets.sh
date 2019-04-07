db14b8659f08c8621698d43af11c769c8b375797
Kristen Newbury <knewbury@ualberta.ca>
2019-04-07 00:21:02 -0600

    add doc to splitsnip script and change split identifier in sqlite query to be more profes

--- splitsnippets.sh
+++ splitsnippets.sh
@@ -1,10 +1,24 @@
+##########################
+#creates a set of files
+#each containing one snippet
+#better to do this here than in db query
+#bc it will be faster and more reusable
+#
+#
+# REQUIRES GNU csplit
+# BSD split limit behaviour is different 
+##########################
+
+
 #first use the split identifier to split content into uniqe code snippets 
 echo "SPLITTING $1"
-csplit -n 5 $1 '/---KNEWBURY---/' {1407} #&>/dev/null
+csplit -n 5 $1 '/---UNIQUEIDFORSNIP---/' {1407} #&>/dev/null
+
 #then remove this identifier
 for file in xx*;
 do
-    sed -i '' 's/---KNEWBURY---//' $file    
+    #for linux: sed -i 's/---UNIQUEIDFORSNIP---//' $file
+    sed -i '' 's/---UNIQUEIDFORSNIP---//' $file    
 done
 
 echo -n "Created this many snippets: "
