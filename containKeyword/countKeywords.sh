40a937f3369a4406505d1a6df21de46423423872
Kristen Newbury <knewbury@ualberta.ca>
2019-04-11 15:08:54 -0600

    countKeywords now prints the total of its count column

--- containKeyword/countKeywords.sh
+++ containKeyword/countKeywords.sh
@@ -7,14 +7,16 @@
 #######################
 declare -a arr=("Provider" "Security" "SecureRandom" "MessageDigest" "Signature" "Cipher" "KeyFactory" "KeyGenerator" "KeyAgreement" "KeyStore" "CertificateFactory" "KeyPair" "KeySpec" "AlgorithmParameter")
 
-mkdir wordCountTable.txt
-
-
+rm wordCountTable.txt
+total=0
 for keyword in "${arr[@]}"; do 
 
     echo -n "$keyword=" >> wordCountTable.txt
-    grep -l -R --include="xx*.java" "$keyword" . | wc -l >> wordCountTable.txt
-    
+    grep -l -R --include="xx*.java" " $keyword " . | wc -l >> wordCountTable.txt
+    add=$(grep -l -R --include="xx*.java" " $keyword " . | wc -l)
+    total=$((total+add))
   done
+
+echo "The total occurrence of keywords is: $total"
 
 RScript genWordCountTable.r wordCountTable.txt
