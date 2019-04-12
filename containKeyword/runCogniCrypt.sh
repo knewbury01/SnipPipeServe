0c1f4970892a539e26503c96a8a2b7913c43cb1e
Kristen Newbury <knewbury@ualberta.ca>
2019-04-12 16:28:54 -0600

    totally revamp run cogni script to use relative paths and add resources to do that from this dir

--- containKeyword/runCogniCrypt.sh
+++ containKeyword/runCogniCrypt.sh
@@ -5,34 +5,16 @@
 mkdir CogniCryptResults
 mkdir isolateclass
 
-for file in compileClass/*.class; do
+declare -a dir=("Original" "compileClass" "compileMethodClass")
 
-    justfile=${file#compileClass/}
+for dir in "${dir[@]}";do
+for file in $dir/*.class; do
+
+    justfile=${file#$dir/}
     echo "RUNNING ON $justfile"
     cp $file isolateclass/$justfile
-    java -cp /Users/knewbury/Desktop/CryptoAnalysis/CryptoAnalysis/build/CryptoAnalysis-2.1-SNAPSHOT-jar-with-dependencies.jar crypto.HeadlessCryptoScanner --rulesDir=/Users/knewbury/Desktop/CryptoAnalysis/JCA_rules --applicationCp=/Users/knewbury/Desktop/snippets/containKeyword/isolateclass ${justfile%.class} &> CogniCryptResults/${justfile%.class}.txt
+    java -cp $(pwd)/CryptoAnalysis-2.1-SNAPSHOT-jar-with-dependencies.jar crypto.HeadlessCryptoScanner --rulesDir=$(pwd)/JCA_rules --applicationCp=$(pwd)/isolateclass ${justfile%.class} &> CogniCryptResults/${justfile%.class}.txt
     rm isolateclass/$justfile
     
 done
-
-
-for file in compileMethodClass/*.class; do
-
-    justfile=${file#compileMethodClass/}
-    echo "RUNNING ON $justfile"
-    cp $file isolateclass/$justfile
-    java -cp /Users/knewbury/Desktop/CryptoAnalysis/CryptoAnalysis/build/CryptoAnalysis-2.1-SNAPSHOT-jar-with-dependencies.jar crypto.HeadlessCryptoScanner --rulesDir=/Users/knewbury/Desktop/CryptoAnalysis/JCA_rules --applicationCp=/Users/knewbury/Desktop/snippets/containKeyword/isolateclass ${justfile%.class} &> CogniCryptResults/${justfile%.class}.txt
-    rm isolateclass/$justfile
-
 done
-
-for file in Original/*.class; do
-
-    justfile=${file#Original/}
-    echo "RUNNING ON $justfile"
-    cp $file isolateclass/$justfile
-
-    java -cp /Users/knewbury/Desktop/CryptoAnalysis/CryptoAnalysis/build/CryptoAnalysis-2.1-SNAPSHOT-jar-with-dependencies.jar crypto.HeadlessCryptoScanner --rulesDir=/Users/knewbury/Desktop/CryptoAnalysis/JCA_rules --applicationCp=/Users/knewbury/Desktop/snippets/containKeyword/isolateclass ${justfile%.class} &> CogniCryptResults/${justfile%.class}.txt
-    rm isolateclass/$justfile
-    
-    done
