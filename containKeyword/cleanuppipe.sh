b8104475df975b7744df14bbea93f159a38c9924
Kristen Newbury <knewbury@ualberta.ca>
2019-04-12 20:19:17 -0600

    allow cleanup pipe to rm classes , for full pipe stages clean

--- containKeyword/cleanuppipe.sh
+++ containKeyword/cleanuppipe.sh
@@ -10,4 +10,7 @@ rm compileClass/*
 rm compileMethodClass/*
 rm outputs/*
 rm X*
-rm classes/*
+#this may contain dirs bc of oddities in renaming attempts
+rm -rf classes/*
+#this one is just an emacs leftover clean, not... safest, but using git so! 
+rm *~
